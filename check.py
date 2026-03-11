ba8b513bff26d897ad86540319fa7dd04ed63fa7
Mohit <mohitkpatwari2005@gmail.com>
2026-03-12 02:28:52 +0530

    chore: remove temp helper scripts

--- check.py
+++
@@ -1,5 +0,0 @@
-﻿import tests.risk_scorer.test_rules as m
-import inspect
-fns = [name for name, obj in inspect.getmembers(m) if inspect.isfunction(obj) and name.startswith('test_')]
-print(f'Found {len(fns)} test functions:')
-for f in fns: print(' ', f)
